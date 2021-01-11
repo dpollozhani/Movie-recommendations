f70b1749a1be7cdaea00158f5dec64e383da773f
Drilon Pollozhani <drilonpollozhani@Drilons-MBP.lan>
2021-01-11 17:55:41 +0100

    Minor modifications to streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from recommendation import generate_recommendations
-import random 
+import random
+from pyngrok import ngrok
+
+public_url = ngrok.connect('8501')
+print(public_url)
 
 def make_clickable_title(df):
     text, link = df['Title'], df['IMDB page']
@@ -20,9 +24,16 @@ def engine(title_type='movie'):
     df['Where to watch'] = df['Where to watch'].apply(make_clickable_link)
     
     return df
-    
-st.title('Pana$onic 2001 recommendations')
-#st.subheader("It's cached and super fast!")
+
+title = '''
+    <div style ="background-color:white;padding:10px">
+    <h1 style ="color:black;text-align:center;">Pana$onic 2001 recommendations</h1>
+    <p style ="background-color:#b991cf;color:orange;font-style:oblique;text-align:center;">Originalni Ohridski Biseri</p>
+    </div> 
+'''
+
+st.markdown(title, unsafe_allow_html=True)
+
 selected_type = st.radio(
     'Select title type',
     ('Movies', 'Series')
@@ -32,9 +43,13 @@ selection_map = {'Movies': 'movie',
                 'Series': 'tvSeries'}
 title_type = selection_map[selected_type]
 
-df = engine(title_type)
-df = df.to_html(escape=False, index=False)
+run = st.button('Get recommendations')
 
-st.write(df, unsafe_allow_html=True)
+if run:
+    with st.spinner('Take a sip of your macchiato while this remarkable beast of an engine does its work.'):
+        #seed = random.randint(1,50)
+        df = engine(title_type)
+        df = df.to_html(escape=False, index=False)
 
+        st.write(df, unsafe_allow_html=True)
 
